ae0a408ae115848eeb965ff310f5138651884fe5
Valiullin <valiullin@localhost.localdomain>
2014-05-27 17:19:51 +0400

    draft. trying other js encrytion module

--- altauth/api.py
+++ altauth/api.py
@@ -1,11 +1,36 @@
 # coding=utf-8
-def rsa_gen_keys(bits=1024):
-    """
-    @param: bits - Key length, in bits
-    @return: Tuple of (secret, public) new RSA keys in PEM format
-    """
-    import rsa
+import rsa
+import base64
 
-    return rsa.newkeys(bits)
 
-PEM_MARKER_FORMAT = u"-----BEGIN RSA PUBLIC KEY-----\n%s\n-----END RSA PUBLIC KEY-----\n"
+class RSAWrapper(object):
+    u""" We can work with rsa"""
+    PEM_MARKER_FORMAT = u"-----BEGIN RSA PUBLIC KEY-----\n%s\n-----END RSA PUBLIC KEY-----\n"
+
+    def __init__(self):
+        super(RSAWrapper, self).__init__()
+        self._p_k = None
+        self._pr_k = None
+        self.rsa_gen_keys(1024)
+
+    def get_public_key(self):
+        u""" returns public key PEM """
+        return self._p_k.save_pkcs1()
+
+    def get_private_key(self):
+        u""" returns private key PEM """
+        return self._pr_k.save_pkcs1()
+
+    def rsa_gen_keys(self, bits=1024):
+        """
+        @param: bits - Key length, in bits
+        @return: Tuple of (secret, public) new RSA keys in PEM format
+        """
+        self._p_k, self._pr_k = rsa.newkeys(bits)
+
+        return self.get_public_key(), self.get_private_key()
+
+    def decrypt_str(self, encoded_str):
+        str = base64.b64decode(encoded_str)
+        decrypted_str = rsa.decrypt(str, self._pr_k)
+        return decrypted_str
--- polling/views.py
+++ polling/views.py
@@ -1,16 +1,18 @@
 # coding=utf-8
 import json
-from altauth.api import rsa_gen_keys
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import logout
-
 from settings import LOGOUT_REDIRECT_URL
 from django.contrib.auth.models import User
 from django.template import RequestContext
-from polling.forms import UserRegistrationForm
 
+from polling.forms import UserRegistrationForm
+from altauth.api import RSAWrapper
+
+rsa_wrapper = RSAWrapper()
 
 @login_required
 def index(request):
@@ -23,15 +25,17 @@ def log_out(request):
 
 
 def register(request):
-    p_k, pr_k = rsa_gen_keys(128)
+    p_k = rsa_wrapper.get_public_key()
 
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
-        if form.is_valid():
+        #if form.is_valid():
             # create new user
-            params = json.loads(request.POST[u'encryptedTextDict'])
-            return HttpResponseRedirect('/')
-    p_k_str = p_k.save_pkcs1().split('\n')[1]
+        params = json.loads(request.POST[u'encryptedTextDict'])
+        for param in params:
+            decrypted = rsa_wrapper.decrypt_str(params[param])
+        return HttpResponseRedirect('/')
+    p_k_str = p_k.split('\n')[1]
     p_k_num = p_k_str
     data = {'public_key': p_k_num}
     form = UserRegistrationForm(data)
